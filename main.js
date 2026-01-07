274bc00744be48f425fe72781fe077c54dfec61a
Julia Prado <juliaplg@hotmail.com>
2026-01-07 04:54:40 -0300

    quest log page!

--- main.js
+++ main.js
@@ -101,3 +101,7 @@ cycleBtn.addEventListener("click", () => {
     
 });
 
+function openTwoLinks(url1,url2) {
+    window.open(url1, '_blank', "noreferrer");
+    window.open(url2, '_blank', "noreferrer").focus();
+}
